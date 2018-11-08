a5e96726474bd523457bc5564a709e289a286f8e
Santiago Gil <santiago.gilt96@gmail.com>
2018-11-08 17:10:16 -0400

    model's names changed to english

--- coevaluador/models.py
+++ coevaluador/models.py
@@ -35,31 +35,32 @@ class Respuestas(models.Model):
     id_pregunta= models.ForeignKey(Preguntas, on_delete=models.CASCADE())
     nota= models.FloatField()
 
-class EquipoTrabajo(models.Model):
-    id_equipo = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE())
+class WorkTeams(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=50)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE())
 
     def __str__(self):
-        return self.nombre
+        return self.team_name
 
-class IntegrantesEquipo(models.Model):
-    id_equipo = models.ForeignKey(EquipoTrabajo, related_name='id_equipo', on_delete=models.CASCADE())
-    nombre_equipo = models.ForeignKey(EquipoTrabajo, related_name='nombre', on_delete=models.CASCADE())
-    rut_estudiante = models.ForeignKey(PersonaNatural, related_name='rut', on_delete=models.CASCADE())
-    nombre_estudiante = models.ForeignKey(PersonaNatural, related_name='nombre', on_delete=models.CASCADE())
+class TeamMembers(models.Model):
+    team_id = models.ForeignKey(WorkTeams, related_name='team_id', on_delete=models.CASCADE())
+    team_name = models.ForeignKey(WorkTeams, related_name='team_name', on_delete=models.CASCADE())
+    student_rut = models.ForeignKey(NaturalPerson, related_name='rut', on_delete=models.CASCADE())
+    student_name = models.ForeignKey(NaturalPerson, related_name='name', on_delete=models.CASCADE())
 
-class HistorialEquipos(models.Model):
-    id_equipo = models.ForeignKey(EquipoTrabajo, on_delete=models.CASCADE())
-    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE())
-    rut_estudiante = models.ForeignKey(PersonaNatural, related_name='rut', on_delete=models.CASCADE())
-    nombre_estudiante = models.ForeignKey(PersonaNatural, related_name='nombre', on_delete=models.CASCADE())
+class TeamHistory(models.Model):
+    team_id = models.ForeignKey(WorkTeams, on_delete=models.CASCADE())
+    team_name = models.ForeignKey(WorkTeams, related_name='team_name', on_delete=models.CASCADE())
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE())
+    student_rut = models.ForeignKey(NaturalPerson, related_name='rut', on_delete=models.CASCADE())
+    student_name = models.ForeignKey(NaturalPerson, related_name='name', on_delete=models.CASCADE())
 
-class Administrador(models.Model):
-    rut_admin = models.CharField(max_length=12, primary_key=True)
-    nombre_admin = models.CharField(max_length=35)
-    apellido_admin = models.CharField(max_length=35)
-    contraseña = forms.CharField(widget=forms.PasswordInput)
+class Admin(models.Model):
+    admin_rut = models.CharField(max_length=12, primary_key=True)
+    admin_name = models.CharField(max_length=35)
+    admin_surname = models.CharField(max_length=35)
+    password = forms.CharField(widget=forms.PasswordInput)
 
     def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        return '%s %s' % (self.admin_name, self.admin_surname)
