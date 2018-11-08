6e5adcc271605da8dab21e380df096d6a062390d
Santiago Gil <santiago.gilt96@gmail.com>
2018-11-08 11:04:03 -0400

    equipo, integrantes, historial, admin models created

--- coevaluador/models.py
+++ coevaluador/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.forms import forms
+
+
 class Coevaluacion(models.Model):
     id_coev=models.CharField(max_length=50)
     n_preguntas= models.IntegerField()
@@ -31,3 +34,32 @@ class Respuestas(models.Model):
     id_evaluado=models.ForeignKey(PersonaNatural, on_delete=models.CASCADE())
     id_pregunta= models.ForeignKey(Preguntas, on_delete=models.CASCADE())
     nota= models.FloatField()
+
+class EquipoTrabajo(models.Model):
+    id_equipo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.nombre
+
+class IntegrantesEquipo(models.Model):
+    id_equipo = models.ForeignKey(EquipoTrabajo, related_name='id_equipo', on_delete=models.CASCADE())
+    nombre_equipo = models.ForeignKey(EquipoTrabajo, related_name='nombre', on_delete=models.CASCADE())
+    rut_estudiante = models.ForeignKey(PersonaNatural, related_name='rut', on_delete=models.CASCADE())
+    nombre_estudiante = models.ForeignKey(PersonaNatural, related_name='nombre', on_delete=models.CASCADE())
+
+class HistorialEquipos(models.Model):
+    id_equipo = models.ForeignKey(EquipoTrabajo, on_delete=models.CASCADE())
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE())
+    rut_estudiante = models.ForeignKey(PersonaNatural, related_name='rut', on_delete=models.CASCADE())
+    nombre_estudiante = models.ForeignKey(PersonaNatural, related_name='nombre', on_delete=models.CASCADE())
+
+class Administrador(models.Model):
+    rut_admin = models.CharField(max_length=12, primary_key=True)
+    nombre_admin = models.CharField(max_length=35)
+    apellido_admin = models.CharField(max_length=35)
+    contraseña = forms.CharField(widget=forms.PasswordInput)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
