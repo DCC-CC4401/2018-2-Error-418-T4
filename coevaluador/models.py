54b878dc70d8f4de71dd0ec0d585786e714d162c
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-11-08 17:33:55 -0300

    refactoring

--- coevaluador/models.py
+++ coevaluador/models.py
@@ -5,7 +5,7 @@ from django import forms
 
 
 class NaturalPerson(models.Model):
-    rut = models.CharField(max_length=50)
+    rut = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
@@ -27,49 +27,49 @@ class TeachingTeamMember(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(widget=forms.PasswordInput)
     rol = models.CharField(max_length=100)
-    course = models.ForeignKey(Course)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
 class Coevaluation(models.Model):
-    id_coev = models.CharField(max_length=50)
-    n_preguntas = models.IntegerField()
-    estado = models.CharField(max_length=50)
-    f_inicio = models.DateField()
-    f_termino = models.DateField()
-    id_curso = models.ForeignKey(Course, on_delete=models.CASCADE())
+    id = models.CharField(max_length=100, primary_key=True)
+    n_questions = models.IntegerField()
+    status = models.CharField(max_length=50)
+    s_date = models.DateField()
+    e_date = models.DateField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
 # Segun yo esto no tiene mucho sentido, pero por mientras lo dejaremos así para ver como sale
 class CourseRecordForStudent(models.Model):
-    student = models.ForeignKey(NaturalPerson)
-    course = models.ForeignKey(Course)
+    student = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     rol = models.CharField(max_length=100)
-    coevaluation = models.ForeignKey(Coevaluation)
+    coevaluation = models.ForeignKey(Coevaluation, on_delete=models.CASCADE)
 
 
 class Team(models.Model):
     pass
 
 
-class FichaCoevaluacion(models.Model):
+class CoevaluationSheet(models.Model):
     id = models.CharField(max_length=50)
-    id_equipo = models.ForeignKey(Team, on_delete=models.CASCADE())
-    id_curso = models.ForeignKey(Course, on_delete=models.CASCADE())
-    id_coev = models.ForeignKey(Coevaluation, on_delete=models.CASCADE())
-    id_alumno = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE())
-    estado = models.ForeignKey(Coevaluation, on_delete=models.CASCADE())
-    nota = models.FloatField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    coevaluation = models.ForeignKey(Coevaluation, on_delete=models.CASCADE)
+    student = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    grade = models.FloatField()
 
 
-class Preguntas(models.Model):
-    id_pregunta = models.CharField(max_length=50)
-    pregunta = models.CharField(max_length=500)
-    id_curso = models.ForeignKey(Course, on_delete=models.CASCADE())
+class Question(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    question = models.CharField(max_length=500)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
-class Respuestas(models.Model):
-    id_coevaluacion = models.ForeignKey(Coevaluation, on_delete=models.CASCADE())
-    id_evaluador = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE())
-    id_evaluado = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE())
-    id_pregunta = models.ForeignKey(Preguntas, on_delete=models.CASCADE())
-    nota = models.FloatField()
+class Answer(models.Model):
+    coevaluation = models.ForeignKey(Coevaluation, on_delete=models.CASCADE)
+    evaluator = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE)
+    evaluated = models.ForeignKey(NaturalPerson, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    grade = models.FloatField()
