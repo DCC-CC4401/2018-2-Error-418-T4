6c9b972e405dd61f4c759332bc9d7b2b8e8f8d34
Matías Pizarro <xof96@github.com>
2018-11-11 20:05:04 -0300

    forms implemented in login

---
+++ coevaluador/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    rut = forms.CharField(max_length=50,
+                          widget=forms.TextInput(attrs={'id': 'input-user',
+                                                        'class': 'form-control',
+                                                        'placeholder': 'RUT',
+                                                        'required': True,
+                                                        'autofocus': True}))
+    
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'id': 'input-password',
+                                                                 'class': 'form-control',
+                                                                 'placeholder': 'Contraseña',
+                                                                 'required': True}))
--- coevaluador/views.py
+++ coevaluador/views.py
@@ -1,11 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import LoginForm
 
 
 # Use underscore separated words for views like hello_world.
 
 
 def login(request):
-    return render(request, 'coevaluador/login.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+
+            rut = form.cleaned_data['rut']
+            password = form.cleaned_data['password']
+            # if ()
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('coevaluador:studentHome'))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, 'coevaluador/login.html', {'form': form})
 
 
 def student_home(request):
