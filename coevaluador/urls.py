ecbdb58ccfceb5bc7a82ea7788cdc3f0aab44c6c
Matías Pizarro <xof96@github.com>
2018-11-20 20:25:55 -0300

    student course almost finished, teaching course updated

--- coevaluador/urls.py
+++ coevaluador/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('teaching-home', views.teaching_home, name='teachingHome'),
     path('student-coevaluation', views.student_coevaluation, name='studentCoevaluation'),
     path('teaching-coevaluation', views.teaching_coevaluation, name='teachingCoevaluation'),
+    path('course/<int:year>/<int:semester>/<str:code>/<int:section>', views.course, name='course'),
     path('student-course', views.student_course, name='studentCourse'),
     path('teaching-course', views.teaching_course, name='teachingCourse'),
     path('owner-profile', views.owner_profile, name='ownerProfile'),
